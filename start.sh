582d1e431e29031c962faff5640a1124407c2e00
aisijimo888 <127577828+aisijimo888@users.noreply.github.com>
2026-03-24 20:48:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,17 +25,17 @@ quicktunnel() {
     case "$ARCH" in
         x86_64|amd64)
             ECH_URL="https://github.com/webappstars/ech-hug/releases/download/3.0/ech-tunnel-linux-amd64"
-            OPERA_URL="https://github.com/Snawoot/opera-proxy/releases/latest/download/opera-proxy.linux-amd64"
+            OPERA_URL="https://github.com/Alexey71/opera-proxy/releases/download/v1.15.0/opera-proxy.linux-amd64"
             CLOUDFLARED_URL="https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64"
             ;;
         i386|i686)
             ECH_URL="https://github.com/webappstars/ech-hug/releases/download/3.0/ech-tunnel-linux-386"
-            OPERA_URL="https://github.com/Snawoot/opera-proxy/releases/latest/download/opera-proxy.linux-386"
+            OPERA_URL="https://github.com/Alexey71/opera-proxy/releases/download/v1.15.0/opera-proxy.linux-386"
             CLOUDFLARED_URL="https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386"
             ;;
         arm64|aarch64)
             ECH_URL="https://github.com/webappstars/ech-hug/releases/download/3.0/ech-tunnel-linux-arm64"
-            OPERA_URL="https://github.com/Snawoot/opera-proxy/releases/latest/download/opera-proxy.linux-arm64"
+            OPERA_URL="https://github.com/Alexey71/opera-proxy/releases/download/v1.15.0/opera-proxy.freebsd-arm64"
             CLOUDFLARED_URL="https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64"
             ;;
         *)
